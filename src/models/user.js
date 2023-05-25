2d1358dbe686e0ceaf9d1bd5d9ebdb7f337bfe3f
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-25 10:28:06 +0530

    Added token for signup and login APIs for user

--- src/models/user.js
+++ src/models/user.js
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema({
     },
     tokens: [
         {
-            tokens: {
+            token: {
                 type: String,
                 required: true
             }
