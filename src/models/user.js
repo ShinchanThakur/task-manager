2aeeb96b0e3f4edc26ca679fc3ce8b628521fa0b
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-26 06:20:13 +0530

    Removed unnecessary fields(password, tokens, versions) before sending user as response

--- src/models/user.js
+++ src/models/user.js
@@ -51,6 +51,17 @@ const userSchema = new mongoose.Schema({
     ]
 });
 
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    delete userObject.password;
+    delete userObject.tokens;
+    delete userObject.__v;
+
+    return userObject;
+}
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
     const payload = {
