bcaa66ae8e69b5e7142820a22a2ef20e87d61349
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-30 04:30:03 +0530

    Added authorization to get task by id

--- src/routers/task.js
+++ src/routers/task.js
@@ -28,10 +28,10 @@ router.get('/tasks', auth, async (req, res) => {
     }
 });
 
-router.get('/tasks/:id', async (req, res) => {
-    const id = req.params.id;
+router.get('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id;
     try {
-        const task = await Task.findById(id);
+        const task = await Task.findOne({ _id, owner: req.user._id });
         if (!task) {
             return res.status(404).send();
         }
