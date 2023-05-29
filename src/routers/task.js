9a3076c3b5db437a05527bd08719f35c40f6d94f
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-30 04:50:44 +0530

    Added authorization to delete task

--- src/routers/task.js
+++ src/routers/task.js
@@ -63,10 +63,10 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 });
 
-router.delete('/tasks/:id', async (req, res) => {
-    const id = req.params.id;
+router.delete('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id;
     try {
-        const task = await Task.findByIdAndDelete(id);
+        const task = await Task.findOneAndDelete({ _id, owner: req.user._id });
         if (!task) {
             return res.status(404).send();
         }
