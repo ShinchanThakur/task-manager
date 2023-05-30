6caf709afd29c3e7bc68e2259ddc6f3be403de98
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-30 05:34:27 +0530

    Cascaded delete user and delete task to delete all tasks related to a user when user is deleted

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -98,6 +99,12 @@ userSchema.pre('save', async function (next) {
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8);
     }
+    next();
+});
+
+userSchema.pre('deleteOne', async function (next) {
+    const user = this._conditions;
+    await Task.deleteMany({ owner: user._id });
     next();
 });
 
