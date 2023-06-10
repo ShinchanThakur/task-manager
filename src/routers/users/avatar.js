c3361a6396c042bda1964a336f8fc976a66f7be6
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-06-10 12:53:18 +0530

    Added get request to get avatar by user id

--- src/routers/users/avatar.js
+++ src/routers/users/avatar.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const auth = require('../../middleware/auth');
 const upload = require('../../middleware/upload');
+const User = require('../../models/user');
 
 const router = new express.Router();
 
@@ -19,6 +20,20 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     user.avatar = undefined;
     await user.save();
     res.send();
-})
+});
+
+router.get('/users/:id/avatar', auth, async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findById(id);
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+    } catch (error) {
+        res.status(404).send();
+    }
+});
 
 module.exports = router;
