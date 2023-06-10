40a00d8e3d23e05f4e6356ad1929062dbebf1069
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-06-10 08:42:41 +0530

    Handling errors thrown from middleware

--- src/routers/users/avatar.js
+++ src/routers/users/avatar.js
@@ -6,6 +6,8 @@ const router = new express.Router();
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
     res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
 });
 
 module.exports = router;
