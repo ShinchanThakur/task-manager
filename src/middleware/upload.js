203b10f4c75acba67331306d5116d03a8bce78e5
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-06-10 09:22:58 +0530

    Adding image to user profile

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,15 +1,15 @@
 const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        const fileExtension = file.originalname.split('.').pop();
-        cb(null, 'avatar-' + uniqueSuffix + '.' + fileExtension); // Generate a unique filename for the uploaded image
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads/');
+//     },
+//     filename: function (req, file, cb) {
+//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//         const fileExtension = file.originalname.split('.').pop();
+//         cb(null, 'avatar-' + uniqueSuffix + '.' + fileExtension); // Generate a unique filename for the uploaded image
+//     }
+// });
 
 const limits = {
     fileSize: 1000000
@@ -23,6 +23,6 @@ const fileFilter = function (req, file, cb) {
     cb(undefined, true);
 };
 
-const upload = multer({ storage, fileFilter, limits });
+const upload = multer({ fileFilter, limits });
 
 module.exports = upload;
--- src/routers/users/avatar.js
+++ src/routers/users/avatar.js
@@ -5,6 +5,10 @@ const upload = require('../../middleware/upload');
 const router = new express.Router();
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const user = req.user;
+    const buffer = req.file.buffer;
+    user.avatar = buffer;
+    await user.save();
     res.send();
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message });
