9336af27d3174d95c74048fb3019c89eb0c41b7b
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-06-14 19:30:22 +0530

    Added unit tests for Delete user

--- tests/routers/users/user.test.js
+++ tests/routers/users/user.test.js
@@ -94,7 +94,27 @@ describe('User Router', () => {
                     invalidField: 'Anything'
                 })
                 .expect(400);
-        })
+        });
+    });
+
+    describe('Delete user', () => {
+        it('should delete account for user', async () => {
+            await request(app)
+                .delete('/users/me')
+                .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+                .send()
+                .expect(200);
+
+            const user = await User.findById(userOne._id);
+            expect(user).toBeNull;
+        });
+
+        it('should not delete account for unauthenticated user', async () => {
+            await request(app)
+                .delete('/users/me')
+                .send()
+                .expect(401);
+        });
     });
 
     afterAll(closeDBConnection);
