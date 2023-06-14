05462eaaaac7e03e883a0d9c5899fb240cfccc60
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-06-14 17:19:43 +0530

    Added positive test for user login

--- tests/routers/users/user.test.js
+++ tests/routers/users/user.test.js
@@ -2,14 +2,29 @@ const request = require('supertest');
 const app = require('../../../src/app');
 const User = require('../../../src/models/user');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+    _id: userOneId,
+    name: 'User1',
+    email: 'user1@mail.com',
+    password: '12345678',
+    tokens: [
+        {
+            token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+        }
+    ]
+}
 
 describe('User Router', () => {
     beforeEach(async () => {
         await User.deleteMany();
+        await new User(userOne).save();
     });
 
     describe('Signup', () => {
-        it('Should signup a new user', async () => {
+        it('should signup a new user', async () => {
             const response = await request(app).post('/users').send({
                 name: 'Rohan',
                 email: 'rohan@example.com',
@@ -33,7 +48,20 @@ describe('User Router', () => {
             //Password encryption assertion
             expect(user.password).not.toBe('12345678');
         });
-    })
+    });
+
+    describe('Login', () => {
+        it('should login existing user', async () => {
+            const response = await request(app).post('/users/login').send({
+                email: userOne.email,
+                password: userOne.password
+            }).expect(200);
+            const user = await User.findById(userOne._id);
+            const dbLatestToken = user.tokens[user.tokens.length - 1].token;
+            const responseToken = response.body.token;
+            expect(responseToken).toBe(dbLatestToken);
+        });
+    });
 
     afterAll(async () => {
         await mongoose.connection.close();
